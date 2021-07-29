49f9853cfc384691f780f17b8ee1bc5ce704e754
ShunyaNagashige <nagashige190441091@gmail.com>
2021-07-30 00:59:50 +0900

    [add] station3

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import './App.css'
  *
  * @type {React.FC}
  */
+
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
+
   return (
     <div>
       <header>Dog アプリ</header>
       <div>犬の画像を表示するサイトです．</div>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
+      <img src={dogUrl}></img>
     </div>
   )
 }
