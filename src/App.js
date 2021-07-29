7d79f7568659f063f8f7723f600f57e92dee54fd
ShunyaNagashige <nagashige190441091@gmail.com>
2021-07-29 18:17:16 +0900

    [add] station3

--- src/App.js
+++ src/App.js
@@ -8,5 +8,11 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  return <header>Dog アプリ</header>
+  return (
+    <div>
+      <header>Dog アプリ</header>
+      <div>犬の画像を表示するサイトです．</div>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
+    </div>
+  )
 }
