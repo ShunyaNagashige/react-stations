6b919a9532a9743ce6d5a939392f5d11ad0bda97
ShunyaNagashige <nagashige190441091@gmail.com>
2021-08-08 20:46:54 +0900

    [add] station9

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,24 @@
+import { useEffect, useState } from 'react'
+
 // DO NOT DELETE
+export const DogListContainer = () => {
+  const [breeds, setBreeds] = useState([])
+
+  useEffect(
+    () =>
+      fetch('https://dog.ceo/api/breeds/list/all')
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          let dogList = []
+          //   data['message'].map(dogList.push(key))
+          for (var key in data['message']) {
+            dogList.push(key)
+          }
+
+          setBreeds(dogList)
+        }),
+    [],
+  )
+}
