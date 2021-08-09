a45c910de90edc94489a00825775b11aca5eeb71
ShunyaNagashige <nagashige190441091@gmail.com>
2021-08-09 17:05:20 +0900

    [fix] station10

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -20,7 +20,9 @@ export const BreedsSelect = props => {
         className="flex-item"
         id="breed-select-box"
         name="breed-select-box"
-        onChange={props.changeHandler}
+        onChange={event => {
+          props.changeHandler(event)
+        }}
       >
         {optionList}
       </select>
